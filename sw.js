672d40c402c42f481d353301313f088e482777cb
TCC-AI <chingkuo@cccl.com.tw>
2025-09-29 19:48:41 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
   // 添加其他需要緩存的資源
 ];
 
